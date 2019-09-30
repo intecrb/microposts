4186912b376955c3dde79cb62fcd484f732bcb5c
awaji-daisuke <awaji_daisuke@intec.co.jp>
2019-09-30 12:41:25 +0900

    Updates

---
+++ precache-manifest.46e33c29303fb51fa592b2e31bec913b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7632735acec418728719eeddb1fcf4a",
+    "url": "/microposts/index.html"
+  },
+  {
+    "revision": "0f108e2f1eb525244618",
+    "url": "/microposts/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "334ffe35d57918317019",
+    "url": "/microposts/static/js/2.9755f5e4.chunk.js"
+  },
+  {
+    "revision": "0f108e2f1eb525244618",
+    "url": "/microposts/static/js/main.005c65c0.chunk.js"
+  },
+  {
+    "revision": "82213f67993c28d1d423",
+    "url": "/microposts/static/js/runtime-main.6f7f3445.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/microposts/static/media/logo.25bf045c.svg"
+  }
+]);
